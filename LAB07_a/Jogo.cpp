1868d87d2afe8b9431e569c255628a33c7ecc9a8
andersonGPPU <112652677+andersonGPPU@users.noreply.github.com>
2022-09-29 10:53:05 -0300

    Update Jogo.cpp

--- LAB07_a/Jogo.cpp
+++ LAB07_a/Jogo.cpp
@@ -4,7 +4,7 @@
 #include "Jogo.h"
 
 Jogo::Jogo() {
-	nome = "nome não digitado"; horas = 0; preco = custo = 0.0f;
+	nome = "nome nÃ£o digitado"; horas = 0; preco = custo = 0.0f;
 }
 Jogo::Jogo(const char* nome, double preco, unsigned int horas) {
 	this->nome = nome;
@@ -19,9 +19,6 @@ void Jogo::atualizar(double preco) {
 	this->preco = preco;
 	calcular();
 }
-void Jogo::calcular() {
-	if(horas > 0) custo = double(preco) / horas;
-}
 void Jogo::jogar(unsigned int horas) {
 	this->horas += horas;
 	calcular();
@@ -30,7 +27,7 @@ void Jogo::exibir_cst() const {
 	std::cout.precision(2);
 	std::cout << std::fixed;
 	std::cout << "\nNOME: " << nome << std::endl;
-	std::cout << "PREÇO: " << preco << std::endl;
+	std::cout << "PREÃ‡O: " << preco << std::endl;
 	std::cout << "HORAS: " << horas << std::endl;
 	std::cout << "CUSTO: " << custo << std::endl;
 }
@@ -38,7 +35,7 @@ void Jogo::exibir() {
 	std::cout.precision(2);
 	std::cout << std::fixed;
 	std::cout << "\nNOME: " << nome << std::endl;
-	std::cout << "PREÇO: " << preco << std::endl;
+	std::cout << "PREÃ‡O: " << preco << std::endl;
 	std::cout << "HORAS: " << horas << std::endl;
 	std::cout << "CUSTO: " << custo << std::endl;
 }
@@ -51,7 +48,7 @@ void Jogo::arquivar() {
 	std::ofstream fout;
 	fout.open("teste", std::ios_base::app | std::ios_base::binary);
 
-	if(fout.is_open()){std::cout << "Iniciando a gravação do arquivo em disco..." << std::endl;
+	if(fout.is_open()){std::cout << "Iniciando a gravaÃ§Ã£o do arquivo em disco... " << nome << std::endl;
 	}
 	else {
 		std::cout << "\nErro ao tentar gravar arquivo!" << std::endl; exit(1);
@@ -62,16 +59,17 @@ void Jogo::arquivar() {
 	fout.write((char*)&preco, sizeof(preco));
 	fout.write((char*)&horas, sizeof(horas));
 	fout.write((char*)&custo, sizeof(custo));
-	fout.close(); std::cout << "Arquivo foi fechado, memória liberada!" << std::endl;
+	fout.close(); std::cout << "Arquivo foi fechado, memÃ³ria liberada!" << std::endl;
 }
 void Jogo::ler_arquivo() {
 	std::ifstream fin;
-	fin.open("teste", std::ios_base::binary);
+	fin.open("teste", std::ios_base::app);
 
-	if (!fin.is_open()) { std::cout << "\nNão foi possível encontrar o arquivo para leitura!" << std::endl; exit(1); }
+	if (!fin.is_open()) { std::cout << "\nNÃ£o foi possÃ­vel encontrar o arquivo para leitura!" << std::endl; exit(1); }
 	else {
+		
 		std::cout << "\niniciando a leitura do arquivo de dados..." << std::endl;
-		while(fin) {
+		while (fin) {
 		fin.read((char*)&nome, sizeof(nome));
 		fin.read((char*)&preco, sizeof(preco));
 		fin.read((char*)&horas, sizeof(horas));
@@ -79,7 +77,6 @@ void Jogo::ler_arquivo() {
 		exibir();
 		}
 	}
-	std::cout << "\nLeitura do arquivo foi concluída!" << std::endl;
-	fin.close();
-	std::cout << "Arquivo foi fechado com memória liberada!" << std::endl;
+	std::cout << "\nLeitura do arquivo foi concluÃ­da!" << std::endl;
+	fin.close(); std::cout << "Arquivo foi fechado com memÃ³ria liberada!" << std::endl;
 }
