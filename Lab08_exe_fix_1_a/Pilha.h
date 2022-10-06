f3d777cd6c2cfb1dc2e9b46023a3cd97a9e77f7f
andersonGPPU <112652677+andersonGPPU@users.noreply.github.com>
2022-10-06 13:58:30 -0300

    Update Pilha.h

--- Lab08_exe_fix_1_a/Pilha.h
+++ Lab08_exe_fix_1_a/Pilha.h
@@ -5,17 +5,17 @@ typedef char Item; //using Item = char;
 
 class Pilha {
 private:
-	size_t tam; //tamanho m·ximo
+	size_t tam; //tamanho m√°ximo
 	Item *itens; //armazenamento de itens
-	size_t topo; //Ìndice do item no topo
+	size_t topo; //√≠ndice do item no topo
 
 public:
 	Pilha(size_t tam); //construtor
 	~Pilha();
 
-	bool Vazia() const; //verifica se est· vazia
+	bool Vazia() const; //verifica se est√° vazia
 
-	Item* Escrever_Item(); //permite ver cada caractere da pilha
+	Item* Escrever_Item() const; //permite ver cada caractere da pilha
 
 	bool Empilhar(const Item&); //adiciona item na pilha
 	bool Desempilhar(Item&); //remove item da pilha
